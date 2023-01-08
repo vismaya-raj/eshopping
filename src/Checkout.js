31762a6d7d0ac9b22726ee25199b3a1d72c0be16
Vismaya <vismayavipz999@gmail.com>
2023-01-08 19:46:32 +0530

    checkoutComponent

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,1 +1,24 @@
 
+import React from "react";
+export function CheckOut()
+{
+    return(
+        <div>
+            <div className="CheckoutProduct">
+            <h3 className="CheckoutProductTitle">Your Orders</h3>
+            </div>
+            <div className="CheckoutPrdctImg">
+                <img src = "https://m.media-amazon.com/images/I/71lhpSraYML._UL1100_.jpg" alt="ZoukBag"/>
+
+            </div>
+            <div>
+                <p><sup>₹</sup>1703</p>
+            </div>
+            <div>
+                <button>Remove from basket</button>
+            </div>
+
+           
+        </div>
+    )
+}
