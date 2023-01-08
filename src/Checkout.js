ebf12144bcd826ac950b819852481c58768af3bf
Vismaya <vismayavipz999@gmail.com>
2023-01-08 20:11:48 +0530

    checkoutComponent

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,24 +1,29 @@
-
 import React from "react";
-export function CheckOut()
-{
-    return(
-        <div>
-            <div className="CheckoutProduct">
-            <h3 className="CheckoutProductTitle">Your Orders</h3>
-            </div>
-            <div className="CheckoutPrdctImg">
-                <img src = "https://m.media-amazon.com/images/I/71lhpSraYML._UL1100_.jpg" alt="ZoukBag"/>
-
-            </div>
-            <div>
-                <p><sup>₹</sup>1703</p>
-            </div>
-            <div>
-                <button>Remove from basket</button>
-            </div>
-
-           
-        </div>
-    )
+export function CheckOut() {
+  return (
+    <div className="Checkout_product">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1.CB423492668"
+        className="checkout_ad"
+        alt="ad"
+      />
+      <div className="CheckoutProduct_info">
+        <h3 className="CheckoutProductTitle">Your Orders</h3>
+        <p className="CheckoutProduct_title">
+          ZOUK Tote Bags for Women - Handmade Bags for Daily Use - Vegan Leather
+          Handbags with Double Handle - Printed Totes for Women.
+        </p>
+        <img
+          src="https://m.media-amazon.com/images/I/71lhpSraYML._UL1100_.jpg"
+          alt="ZoukBag"
+          className="checkout_image"
+        />
+        <p className="checkout_price">
+          <sup>₹</sup>1703
+        </p>
+      </div>
+      <div className="checkoutproduct_rating">⭐⭐⭐⭐⭐⭐</div>
+      <button className="Checkout_basket">Remove from basket</button>
+    </div>
+  );
 }
