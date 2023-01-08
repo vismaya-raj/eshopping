76c648a5106055333d169e6a9da65d521a5fca74
Vismaya <vismayavipz999@gmail.com>
2023-01-08 23:49:13 +0530

    checkoutcomponent

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./CheckoutProduct.css";
+export function CheckoutProduct() {
+  return (
+    <div className="Checkout_product">
+      <img
+          src="https://m.media-amazon.com/images/I/71lhpSraYML._UL1100_.jpg"
+          alt="ZoukBag"
+          className="checkout_image"
+        />
+      <div className="CheckoutProduct_info">
+       
+        <p className="CheckoutProduct_title">
+          ZOUK Tote Bags for Women - Handmade Bags for Daily Use - Vegan Leather
+          Handbags with Double Handle - Printed Totes for Women.
+        </p>
+        
+        <p className="checkout_price">
+          <sup>₹</sup>1703
+        </p>
+
+        <div className="checkoutproduct_rating">⭐⭐⭐⭐⭐⭐</div>
+      </div>
+      <button className="Checkout_basket">Remove from basket</button>
+    </div>
+  );
+}
